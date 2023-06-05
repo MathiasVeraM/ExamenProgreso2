789e880cde9eb1ea98e5e5a4ac4e07b53cfa27eb
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-06-05 15:25:52 -0500

    Inclusion de las librerias a usar y definicion de la funcion main

---
+++ ExamenProgreso2.c
@@ -0,0 +1,12 @@
+//Las bibliotecas a usar, stdio.h para imprimir valores, time.h y stdlib.h para generar numeros aleatorios
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+//Constante de alumnos y progresos por semestre
+#define alumnos 23
+#define progreso 3
+int main(){
+    srand(time(NULL));
+
+    return 0;
+}
