7dcb0a7c955092246bd76a164ee8cef87c2c63b4
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-06-05 15:46:09 -0500

    El uso de funciones no resultò como yo querìa, aun no las domino, asì que harè los procesos en la funcion principal main, si me sobra tiempo seguirè tratando con otras funciones

--- ExamenProgreso2.c
+++ ExamenProgreso2.c
@@ -5,20 +5,28 @@
 //Constante de alumnos y progresos por semestre
 #define alumnos 23
 #define progreso 3
+int PromedioProgreso(int x){
+    int promedio=x/23;
+    return promedio;
+}
 void CrearMatriz(){
-    int i, j;
+    int i, j, sumaProgreso=0;
     int Semestre[progreso][alumnos];
     for(i=0;i<progreso;i++){
         for(j=0;j<alumnos;j++){
             Semestre[i][j]=rand()%11;
             printf("%d ", Semestre[i][j]);
+            sumaProgreso=Semestre[i][j];
         }
         printf("\n");
+        sumaProgreso=0;
     }
 }
 int main(){
     srand(time(NULL));
+    int sumaProgreso=0;
     CrearMatriz();
-
+    int TotalProgreso = PromedioProgreso(sumaProgreso);
+    printf("El promedio del progreso es %d ", TotalProgreso);
     return 0;
 }
