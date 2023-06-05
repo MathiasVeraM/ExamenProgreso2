13f0eabad58f236ee96b831a94a09e34816e9b20
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-06-05 15:31:48 -0500

    Se incluyò la funciòn Crear Matriz

--- ExamenProgreso2.c
+++ ExamenProgreso2.c
@@ -5,8 +5,20 @@
 //Constante de alumnos y progresos por semestre
 #define alumnos 23
 #define progreso 3
+void CrearMatriz(){
+    int i, j;
+    int Semestre[progreso][alumnos];
+    for(i=0;i<progreso;i++){
+        for(j=0;j<alumnos;j++){
+            Semestre[i][j]=rand()%11;
+            printf("%d ", Semestre[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main(){
     srand(time(NULL));
+    CrearMatriz();
 
     return 0;
 }
